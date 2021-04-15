7e676d6b09b8717ccfca3de7979c534d5c5475a9
Alfeu <36578943+alfeuduran@users.noreply.github.com>
2021-04-15 11:14:47 -0300

    delete extra code

--- AzureFunctionML/HttpPayload/__init__.py
+++ AzureFunctionML/HttpPayload/__init__.py
@@ -8,32 +8,6 @@ import asyncio
 import typing
 
 
-'''
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    
-    url = os.getenv("AKSEndpoint")
-
-    headers = {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {os.getenv("AKSKey")}'}
-
-    data = json.loads(req.get_body())
-    
-    #payload = '{"name":0,"index":["type","amount","oldbalanceOrg","newbalanceOrig","oldbalanceDest","newbalanceDest","hour","dayOfMonth","isMerchantDest","errorBalanceOrig","errorBalanceDest"],"data":[3.0,1864.28,21249.0,19384.72,0.0,0.0,1.0,1.0,1.0,0.0,1864.28]}' 
-
-    response = requests.request("POST", url, headers=headers, data= json.dumps(data))
-    #alterar prediction
-    data.update({"prediction" : response.json()['prediction'][1]})
-    
-    #send to eventHub
-    requests.request("POST", "http://localhost:7071/api/EventHubHttpTrigger", headers=headers, data= json.dumps(data))
-
-
-    return func.HttpResponse(response.text,status_code=200)
-    
-'''
-
 def main(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
     #message = req.params.get('body')
     url = os.getenv("AKSEndpoint")
