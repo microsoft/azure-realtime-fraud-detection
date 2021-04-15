fb023593a449d6db499255b812d2e312d8be95f6
Alfeu <36578943+alfeuduran@users.noreply.github.com>
2021-04-15 11:13:50 -0300

    modify http function to deal with 2 outbounds using bindings and performance adjustments
    modify http function to deal with 2 outbounds using bindings and performance adjustments

--- AzureFunctionML/HttpPayload/__init__.py
+++ AzureFunctionML/HttpPayload/__init__.py
@@ -5,7 +5,10 @@ import json
 import requests
 import os
 import asyncio
+import typing
 
+
+'''
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
@@ -29,3 +32,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     return func.HttpResponse(response.text,status_code=200)
     
+'''
+
+def main(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
+    #message = req.params.get('body')
+    url = os.getenv("AKSEndpoint")
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {os.getenv("AKSKey")}'}
+
+    data = json.loads(req.get_body())
+    response = requests.request("POST", url, headers=headers, data= json.dumps(data))
+    data.update({"prediction" : response.json()['prediction'][1]})
+    msg.set(json.dumps(data))
+
+    return func.HttpResponse(response.text,status_code=200)
